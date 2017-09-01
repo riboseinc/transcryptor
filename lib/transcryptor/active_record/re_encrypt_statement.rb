3bbc14bf87a425c4f6d7290113de4b8111103c01
Jeffrey Lau <jeffrey.lau@ribose.com>
2017-09-01 16:16:58 +0800

    Make ReEncryptStatement#re_encrypt_column take in `extra_columns` like #re_encrypt does

--- lib/transcryptor/active_record/re_encrypt_statement.rb
+++ lib/transcryptor/active_record/re_encrypt_statement.rb
@@ -1,8 +1,8 @@
 module Transcryptor::ActiveRecord::ReEncryptStatement
-  def re_encrypt_column(table_name, attribute_name, old_opts = {}, new_opts = {})
+  def re_encrypt_column(table_name, attribute_name, old_opts = {}, new_opts = {}, extra_columns = [])
     Transcryptor::Instance
       .new(Transcryptor::ActiveRecord::Adapter.new(self))
-      .re_encrypt(table_name, attribute_name, old_opts, new_opts)
+      .re_encrypt(table_name, attribute_name, old_opts, new_opts, extra_columns)
   end
 end
 
