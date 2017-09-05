9ce21db2f59fe1b0ac5d0905798f983b098e4c3d
Jeffrey Lau <jeffrey.lau@ribose.com>
2017-09-05 17:55:49 +0800

    spec: Allow params to #re_encrypt_column to be specified in different contexts

--- spec/transcryptor/active_record/re_encrypt_statement_spec.rb
+++ spec/transcryptor/active_record/re_encrypt_statement_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'securerandom'
 
 ActiveRecord::Base.connection.create_table(:active_record_re_encrypt_statement_specs) do |t|
   t.integer  :lucky_integer, default: 7
@@ -12,65 +13,61 @@ class ActiveRecordReEncryptStatementSpec < ActiveRecord::Base
   attr_encrypted :column_1, key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe'
 end
 
-class ReEncryptActiveRecordReEncryptStatementSpecColumn1 < ActiveRecord::Migration
-  def up
-    re_encrypt_column(
-      :active_record_re_encrypt_statement_specs,
-      :column_1,
-      { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
-      { key: '2asd2asd2asd2asd2asd2asd2asd2asd' },
-    )
-  end
-
-  def down
-    re_encrypt_column(
-      :active_record_re_encrypt_statement_specs,
-      :column_1,
-      { key: '2asd2asd2asd2asd2asd2asd2asd2asd' },
-      { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
-    )
-  end
-end
-
-class ReEncryptActiveRecordReEncryptStatementSpecColumn1WithExtraColumns < ActiveRecord::Migration
-  def up
-    re_encrypt_column(
-      :active_record_re_encrypt_statement_specs,
-      :column_1,
-      { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
-      { key: ->(o) { '2asd2asd2asd2asd2asd2asd2asd2asd'.gsub(/2/, o.lucky_integer.to_s) } },
-      %i[lucky_integer]
-    )
-  end
-
-  def down
-    re_encrypt_column(
-      :active_record_re_encrypt_statement_specs,
-      :column_1,
-      { key: ->(o) { '2asd2asd2asd2asd2asd2asd2asd2asd'.gsub(/2/, o.lucky_integer.to_s) } },
-      { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
-      %i[lucky_integer]
-    )
-  end
-end
-
 describe Transcryptor::ActiveRecord::ReEncryptStatement do
+
+  let(:migration) do
+    Kernel.const_set(
+      :"Migration#{SecureRandom.hex(4)}",
+      Class.new(ActiveRecord::Migration),
+    )
+  end
+
   before do
+    allow(migration).to receive(:up) do
+      migration.instance_exec(up_params) do |params|
+        re_encrypt_column(*params)
+      end
+    end
+
+    allow(migration).to receive(:down) do
+      migration.instance_exec(down_params) do |params|
+        re_encrypt_column(*params)
+      end
+    end
+
     migration.migrate(:up)
     ActiveRecordReEncryptStatementSpec.encrypted_attributes[:column_1][:key] = new_key
   end
+
+  let(:expected_value) { 'my_value' }
+  let!(:record) { ActiveRecordReEncryptStatementSpec.create!(column_1: expected_value) }
 
   after do
     migration.migrate(:down)
     ActiveRecordReEncryptStatementSpec.delete_all
   end
 
-  let(:expected_value) { 'my_value' }
-
   context 'with no extra columns specified' do
-    let!(:record)   { ActiveRecordReEncryptStatementSpec.create!(column_1: expected_value) }
-    let(:migration) { ReEncryptActiveRecordReEncryptStatementSpecColumn1 }
-    let(:new_key)   { '2asd2asd2asd2asd2asd2asd2asd2asd' }
+
+    let(:new_key) { '2asd2asd2asd2asd2asd2asd2asd2asd' }
+
+    let(:up_params) do
+      [
+        :active_record_re_encrypt_statement_specs,
+        :column_1,
+        { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
+        { key: new_key },
+      ]
+    end
+
+    let(:down_params) do
+      [
+        :active_record_re_encrypt_statement_specs,
+        :column_1,
+        { key: new_key },
+        { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
+      ]
+    end
 
     it 'appends #re_encrypt_column to ActiveRecord::Migration instance' do
       expect(record.reload.column_1).to eq(expected_value)
@@ -78,9 +75,28 @@ describe Transcryptor::ActiveRecord::ReEncryptStatement do
   end
 
   context 'with extra columns specified' do
-    let!(:record)   { ActiveRecordReEncryptStatementSpec.create!(column_1: expected_value) }
-    let(:migration) { ReEncryptActiveRecordReEncryptStatementSpecColumn1WithExtraColumns }
-    let(:new_key)   { '7asd7asd7asd7asd7asd7asd7asd7asd' }
+
+    let(:new_key) { '7asd7asd7asd7asd7asd7asd7asd7asd' }
+
+    let(:up_params) do
+      [
+        :active_record_re_encrypt_statement_specs,
+        :column_1,
+        { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
+        { key: ->(o) { '2asd2asd2asd2asd2asd2asd2asd2asd'.gsub(/2/, o.lucky_integer.to_s) } },
+        %i[lucky_integer],
+      ]
+    end
+
+    let(:down_params) do
+      [
+        :active_record_re_encrypt_statement_specs,
+        :column_1,
+        { key: ->(o) { new_key } },
+        { key: '1qwe1qwe1qwe1qwe1qwe1qwe1qwe1qwe' },
+        %i[lucky_integer],
+      ]
+    end
 
     it 'appends #re_encrypt_column to ActiveRecord::Migration instance' do
       expect(record.reload.column_1).to eq(expected_value)
